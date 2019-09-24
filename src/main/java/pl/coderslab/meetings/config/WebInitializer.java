16e1e30ffa95df34a00611d5020c596a476790c9
AdamSzambur <adamszamburski@gmail.com>
2019-09-24 22:40:44 +0200

    Mamy listę dostepnych użytkowników.

---
+++ src/main/java/pl/coderslab/meetings/config/WebInitializer.java
@@ -0,0 +1,15 @@
+package pl.coderslab.meetings.config;
+
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class WebInitializer implements WebApplicationInitializer {
+    // potrzebujemy tego do uzyskania listy aktualnie zalogowanych uzytkowników
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        servletContext.addListener(HttpSessionEventPublisher.class);
+    }
+}
